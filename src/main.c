b8c69a0047c4a44ee15dd40bea9ed9f814ceb6ec
James Wilcock <jameswilcock8@gmail.com>
2024-01-14 23:09:00 -0800

    removed old code in thread splitter

--- src/main.c
+++ src/main.c
@@ -21,77 +21,16 @@
 #include "pulog.h"
 
 #define THREAD_SPLITTER_IMPLEMENTATION
-#include "splitter.h"
+#include "thread_splitter.h"
 
 #include <assert.h>
 #include <string.h>
-typedef struct {
-  int *array;
-  int start;
-  int end;
-  long long sum;
-} TArgs;
-
-// example for using api
-int calculateSum(void *arg) {
-  TArgs *args = (TArgs *)arg;
-  args->sum = 0;
-  for (int i = args->start; i < args->end; i++) {
-    args->sum += args->array[i];
-  }
-  return 0;
-}
-
-int count_primes(void *arg) {
-  ThreadArgs *args = (ThreadArgs *)arg;
-  return 0;
-}
-
-typedef struct par_return {
-  void **data;
-  size_t num_data;
-} par_return;
-
-par_return parallelize(void *data, size_t dataSize, size_t elementSize,
-                       void *extraArgs, TaskFunction func) {
-  int num_cores = get_nprocs();
-  printf("Splitting function amount %d thread\n", num_cores);
-  thrd_t threads[num_cores];
-  ThreadArgs threadArgs[num_cores];
-  void **results = malloc(num_cores * sizeof(void *));
-
-  size_t segmentSize = dataSize / num_cores;
-  assert(dataSize > num_cores);
-  par_return return_struct = {0};
-  for (int i = 0; i < num_cores; i++) {
-    size_t offset = i * segmentSize;
-    threadArgs[i].dataSegment = (char *)data + offset * elementSize;
-    threadArgs[i].segmentSize =
-        (i == num_cores - 1) ? dataSize - offset : segmentSize;
-    threadArgs[i].extraArgs = extraArgs;
-    threadArgs[i].func = func;
-    if (thrd_create(&threads[i], threadFunc, &threadArgs[i]) != thrd_success) {
-      fprintf(stderr, "Error creating thread %d\n", i);
-      free(results);
-      return return_struct;
-    }
-  }
-
-  for (int i = 0; i < num_cores; i++) {
-    thrd_join(threads[i], NULL);
-    results[i] = threadArgs[i].result;
-  }
-  return_struct.data = results;
-  return_struct.num_data = num_cores;
-
-  return return_struct;
-}
 
 void *sumSegment(void *arg) {
   ThreadArgs *threadArgs = (ThreadArgs *)arg;
   int multiplier = *(int *)threadArgs->extraArgs;
-  printf("got arg in thread mul %d\n", multiplier);
-  // Process the segment of data
+  // printf("got arg in thread mul %d\n", multiplier);
+  //  Process the segment of data
   int *dataSegment = (int *)threadArgs->dataSegment;
   long long *sum = malloc(sizeof(long long));
   *sum = 0;
@@ -133,17 +72,7 @@ int main(int argc, char *argv[]) {
       set_loglevel(ERROR);
     }
   }
-  unsigned int num_buckets = 4;
-  unsigned int num_elements = 16;
-  split_info info = split_data(num_elements, num_buckets);
-  assert(info.nominal_amt == 4);
-  assert(info.last_amt == 4);
-  printf("num of els = %zu nominal_amt = %zu\n", info.num_elements,
-         info.nominal_amt);
-  num_elements = 18;
-  info = split_data(num_elements, num_buckets);
-  printf("num of els = %zu nominal_amt = %d last_amt = %d\n", info.num_elements,
-         info.nominal_amt, info.last_amt);
+
   LOG(DEBUG, "test debug log\n");
 
   // Parallelize test
@@ -154,8 +83,8 @@ int main(int argc, char *argv[]) {
   }
   int *multiplier = malloc(sizeof(int));
   *multiplier = 4;
-  par_return results =
-      parallelize(array, arraySize, sizeof(int), multiplier, sumSegment);
+  thread_split_ret results =
+      thread_split(array, arraySize, sizeof(int), multiplier, sumSegment);
   long long totalSum = 0;
   for (int i = 0; i < results.num_data; i++) {
     totalSum += *(long long *)results.data[i];
@@ -167,7 +96,7 @@ int main(int argc, char *argv[]) {
   free(results.data);
   free(multiplier);
 
-  size_t size = 30535700;
+  size_t size = 1000000;
   uint32_t *numarray = malloc(size * sizeof(uint32_t));
   // populate array
   for (size_t i = 0; i < size; i++) {
--- src/thread_splitter.h
+++ src/thread_splitter.h
@@ -4,27 +4,7 @@
 #include <stdlib.h>
 #include <sys/sysinfo.h>
 #include <threads.h>
-typedef struct split_info {
-  size_t num_used_buckets;
-  unsigned int nominal_amt;
-  unsigned int last_amt;
-  size_t num_elements;
-  unsigned int num_buckets;
-} split_info;
 
-typedef struct split_args {
-  void *list_start;
-  size_t bucket_size;
-  size_t return_size;
-  void *args;
-} split_args;
-
-typedef struct split_return {
-  void **ret_data_list;
-  size_t num_returns;
-} split_return;
-
-typedef void *(*TaskFunction)(void *);
 typedef void *(*ThreadSplitFunction)(void *);
 
 typedef struct {
@@ -32,7 +12,7 @@ typedef struct {
   size_t segmentSize;
   void *extraArgs;
   void *result;
-  TaskFunction func;
+  ThreadSplitFunction func;
 } ThreadArgs;
 
 typedef struct thread_split_ret {
@@ -40,7 +20,8 @@ typedef struct thread_split_ret {
   size_t num_data;
 } thread_split_ret;
 
-split_info split_data(unsigned int num_elements, unsigned int num_buckets);
+thread_split_ret thread_split(void *data, size_t dataSize, size_t elementSize,
+                              void *Args, ThreadSplitFunction func);
 #ifdef THREAD_SPLITTER_IMPLEMENTATION
 #include <assert.h>
 #include <stdio.h>
@@ -63,7 +44,8 @@ thread_split_ret thread_split(void *data, size_t dataSize, size_t elementSize,
   // the threads
   int segmentRemainder = dataSize % num_cores;
 
-  assert(dataSize > num_cores);
+  assert(dataSize > num_cores &&
+         "your data is shorter than the number of cores");
   thread_split_ret return_struct = {0};
   size_t offset = 0;
   for (int i = 0; i < num_cores; i++) {
@@ -98,55 +80,6 @@ thread_split_ret thread_split(void *data, size_t dataSize, size_t elementSize,
 
   return return_struct;
 }
-
-split_info split_data(unsigned int num_elements, unsigned int num_buckets) {
-  // find a way to use more efficient splitting
-  // algo , where you spread the remainder over the buckets
-  // rather than adding to last bucket
-  //
-  // for exmaple in this method with 3 buckets and 11 elements
-  // you have a remainder of 2 and can spread as 4,4,3
-  // 3 buckets 10 elements remainder 1 can spread as 4,3,3
-  // increment for this first N%T threads
-  split_info info = {0};
-  info.num_buckets = num_buckets;
-  info.num_elements = num_elements;
-  // check if buckets is greater than elements
-  assert(num_buckets != 0 && "trying to split into 0 buckets");
-  assert(num_elements != 0 && "trying to split 0 elements");
-  if (num_buckets > num_elements) {
-    info.nominal_amt = 1;
-    info.last_amt = 1;
-    info.num_used_buckets = num_elements;
-  }
-  unsigned int remainder = num_elements % num_buckets;
-  unsigned int divided = num_elements / num_buckets;
-  info.nominal_amt = divided;
-  info.last_amt = divided + remainder;
-  info.num_used_buckets = num_buckets;
-  return info;
-}
-
-// change to return a result type that says
-// if there was error
-/*
-split_return split_work(void *list, size_t size_el, size_t num_el,
-                        thrd_start_t func, split_args args) {
-
-  int num_cores = get_nprocs();
-  // allocate number of threads equal to core
-  split_info splits = split_data(num_el, num_cores);
-  thread_t *threads = malloc(splits.num_used_buckets * sizeof(thread_t));
-
-  for (int i = 0; i < splits.num_used_buckets; i++) {
-    if (thrd_create(&threads[i], func, &args) != thrd_success) {
-      fprintf(stderr, "Error creating thread\n");
-      return NULL;
-    }
-  }
-}
-*/
-
 #endif
 
 #endif // !THREAD_SPLITTER_H
